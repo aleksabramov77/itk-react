3b6d292b606a3f14552eef04ba048e773f518677
Aleksandr Abramov <aleksabramov77@gmail.com>
2021-09-03 20:25:24 +0300

    learning CSS modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return <header className='header'>
-    <img src='https://seeklogo.com/images/C/company-name-logo-C74A7D6F5A-seeklogo.com.png' />
-  </header>
+  return (
+    <header className="header">
+      <img src="https://seeklogo.com/images/C/company-name-logo-C74A7D6F5A-seeklogo.com.png" />
+    </header>
+  );
 };
 
 export default Header;
