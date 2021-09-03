bca783abbdcf9f37bb4d2844be70ad0da281d96d
Aleksandr Abramov <aleksabramov77@gmail.com>
2021-09-03 13:47:27 +0300

    Copy project from local

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src='https://seeklogo.com/images/C/company-name-logo-C74A7D6F5A-seeklogo.com.png' />
+  </header>
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return  <div className='content'>
+    <div>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Herjangsfjorden_%26_Ofotfjorden%2C_wide%2C_2009_09.jpg"></img>
+    </div>
+    <div className='avatar'>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg'></img>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          Post 1
+        </div>
+        <div>
+          Post 2
+        </div>
+      </div>
+    </div>
+  </div>
+};
+
+export default Profile;
