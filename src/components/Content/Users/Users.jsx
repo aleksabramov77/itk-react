4a8ed48e313a37726e81fd83b935e5364f6576de
Aleksandr Abramov <aleksabramov77@gmail.com>
2021-09-27 01:00:13 +0300

    51  button getUsers

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -3,58 +3,13 @@ import User from './User/User'
 import * as axios from 'axios'
 
 const Users = (props) => {
-    // if (props.users.length === 0) {
-    //     props.setUsers([
-    //         {
-    //             id: 1,
-    //             followed: false,
-    //             name: 'Tatiana',
-    //             photos: {
-    //                 small: 'https://sun9-16.userapi.com/impg/XS1IlMAFGmZhn7VN7ZK8PgjG0bNzrk9duv-YcQ/LRftUmte2c4.jpg?size=810x1080&quality=96&sign=8ba0ed393577065ee17f42c942728519&type=album',
-    //                 large: null
-    //             },
-    //             location: {
-    //                 country: 'Belarus',
-    //                 city: 'Minsk'
-    //             },
-    //             status: 'I like children !!!'
-    //         },
-    //         {
-    //             id: 2,
-    //             followed: true,
-    //             name: 'Natasha',
-    //             photos: {
-    //                 small: 'https://uralmotorsport.ru/wp-content/uploads/2018/02/default-avatar-f_1920.png',
-    //                 large: null
-    //             },
-    //             location: {
-    //                 country: 'Russia',
-    //                 city: 'Penza'
-    //             },
-    //             status: 'I am a boss !!!'
-    //         },
-    //         {
-    //             id: 3,
-    //             followed: false,
-    //             name: 'Egor',
-    //             photos: {
-    //                 small: 'https://previews.123rf.com/images/sonsedskaya/sonsedskaya1610/sonsedskaya161000024/65029936-portrait-of-funny-cat-scottish-fold-closeup-isolated-on-white-background.jpg',
-    //                 large: null
-    //             },
-    //             location: {
-    //                 country: 'Ukraine',
-    //                 city: 'Kiev'
-    //             },
-    //             status: 'I am a cat !!!'
-    //         }
-    //         ])
-    // }
-
-    if (props.users.length === 0) {
-        axios
-            .get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => props.setUsers(response.data.items)
-            )
+    function getUsers() {
+        if (props.users.length === 0) {
+            axios
+                .get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => props.setUsers(response.data.items)
+                )
+        }
     }
 
     let users = props.users.map(u => <User
@@ -62,6 +17,7 @@ const Users = (props) => {
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {users}
         </div>
     )
