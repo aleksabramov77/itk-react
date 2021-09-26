008ec6f7e367eda860d5485991967bc008d5069a
Aleksandr Abramov <aleksabramov77@gmail.com>
2021-09-27 00:39:14 +0300

    50 axios getUsers

--- src/components/Content/Users/User/User.jsx
+++ src/components/Content/Users/User/User.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import s from './User.module.css'
+import defaultAvatar from '../../../../assets/images/defaultAvatar.png'
 
 const User = (props) => {
 
     return (
         <div className={s.user}>
-            <div className={s.name}>    {props.userData.name}    </div>
+            <div className={s.name}>
+            <img className={s.avatar}  src={props.userData.photos.small ? props.userData.photos.small : defaultAvatar}/>
+                {props.userData.name}
+            </div>
 
-            {/*<div className={s.country}>    {props.userData.location.country}    </div>*/}
-            {/*<div className={s.city}>    {props.userData.location.city}    </div>*/}
+            <div className={s.country}>    {'props.userData.location.country'}    </div>
+            <div className={s.city}>    {'props.userData.location.city'}    </div>
 
             <div className={s.status}>    {props.userData.status}    </div>
 
