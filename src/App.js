3b53bb7e677f66679412c456bf7484e8365200a8
Aleksandr Abramov <aleksabramov77@gmail.com>
2021-10-15 17:24:12 +0300

    95 gh-pages deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from './App.module.css'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Content from './components/Content/Content'
 import HeaderContainer from './components/Header/HeaderContainer'
@@ -17,15 +18,15 @@ class App extends React.Component {
 
     render () {
         if (!this.props.initialized) return <Preloader />
-        // debugger
         return (
-            <BrowserRouter>
+            // <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <HashRouter>
                 <div className={s.appWrapper}>
-                    <div className={s.block + ' ' + s.header}><HeaderContainer/></div>
+                    <d1iv className={s.block + ' ' + s.header}><HeaderContainer/></d1iv>
                     <div className={s.block + ' ' + s.navbar}><Navbar/></div>
                     <div className={s.block + ' ' + s.content}><Content/></div>
                 </div>
-            </BrowserRouter>
+            </HashRouter>
         )
     }
 }
